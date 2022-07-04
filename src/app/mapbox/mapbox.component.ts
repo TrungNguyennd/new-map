8697bc8b2e324b0fed3568b57876839f3ea1d0c4
Namtrung123 <trungcntt96@gmail.com>
2022-07-04 15:43:39 +0700

    lên

--- src/app/mapbox/mapbox.component.ts
+++ src/app/mapbox/mapbox.component.ts
@@ -6,16 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapbox.component.scss'],
 })
 export class MapboxComponent implements OnInit {
-
-  var apiKey = 'w1Dlh2wRon7mE6sL196TgvLS45fw02uon74pJ0rc' // "Nhập khóa API eKMap Platform của bạn"
-  var map = new mapboxgl.Map({
-  container: 'divMapId',center: [105, 17],
-  zoom: 4
-  });
-  var mapOSMBrigth = new ekmapplf.VectorBaseMap('OSM:Bright', apiKey).addTo(map);
   
   constructor() {}
 
   ngOnInit(): void {}
-  
+  var apiKey = 'w1Dlh2wRon7mE6sL196TgvLS45fw02uon74pJ0rc' // "Nhập khóa API eKMap Platform của bạn"
+var map = new mapboxgl.Map({
+container: 'divMapId',center: [105, 17],
+zoom: 4
+});
+var mapOSMBrigth = new ekmapplf.VectorBaseMap('OSM:Bright', apiKey).addTo(map);
 }
