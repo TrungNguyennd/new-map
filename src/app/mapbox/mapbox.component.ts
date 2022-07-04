57aa3d5bf1e0ede2eb27ea6105101cdcaea37046
Namtrung123 <trungcntt96@gmail.com>
2022-07-04 15:06:10 +0700

    lên

--- src/app/mapbox/mapbox.component.ts
+++ src/app/mapbox/mapbox.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapboxComponent implements OnInit {
 
-  var apiKey = 'YOUR_API_KEY' // "Nhập khóa API eKMap Platform của bạn"
+  var apiKey = 'w1Dlh2wRon7mE6sL196TgvLS45fw02uon74pJ0rc' // "Nhập khóa API eKMap Platform của bạn"
   var map = new mapboxgl.Map({
   container: 'divMapId',center: [105, 17],
   zoom: 4
